4d9aa551b560d3f80b4894252adfa79e77313735
liangfapeng <Fredtwins@163.com>
2018-06-22 09:58:11 +0800

    图例

--- src/components/echartsList/listcontent.vue
+++ src/components/echartsList/listcontent.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="listcontent">
     <Breadcrumb class="listtop">
-        <Breadcrumb-item class="black">图形</Breadcrumb-item>
-        <Breadcrumb-item class="blue">图形管理</Breadcrumb-item>
-      </Breadcrumb>
+      <Breadcrumb-item class="black">图形</Breadcrumb-item>
+      <Breadcrumb-item class="blue">图形管理</Breadcrumb-item>
+    </Breadcrumb>
     <Collapse class="aa">
       <Panel name="1">
         条件筛选
@@ -19,7 +19,9 @@
           <Modal v-model="looklists" class="modalx">
             <div slot="header" ref="inter">{{updateTitle}}</div>
             <Form :model="formItem">
-              <Button type="primary">新增</Button>
+              <Button class="addbtn" type="primary" @click="addbtn">雨量</Button>
+              <Button class="addbtn" type="success" @click="fengbtn">风力</Button>
+              <Button class="addbtn" type="error" @click="listbtns">柱形图</Button>
               <div id="main" class="main" style="width: 72rem;height: 530px"></div>
             </Form>
           </Modal>
@@ -95,7 +97,7 @@ export default {
       // console.log(myChart)
       let option = {
         title: {
-          text: 'ECharts 入门示例'
+          text: 'ECharts 图'
         },
         tooltip: {},
         legend: {
@@ -114,8 +116,130 @@ export default {
       myChart.setOption(option);
       // console.log(option);
     },
+    addbtnslist() {
+      let myChart = echarts.init(document.getElementById('main'));
+      let option = {
+        title: {
+          text: '折线图堆叠'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      };
+      myChart.setOption(option);
+    },
     submitSearch() {
 
+    },
+    addbtn() {
+      this.addbtnslist()
+    },
+    fenglist() {
+      let myChart = echarts.init(document.getElementById('main'));
+      let option = {
+        title: {
+          text: '基础雷达图'
+        },
+        tooltip: {},
+        legend: {
+          data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+        },
+        radar: {
+          // shape: 'circle',
+          name: {
+            textStyle: {
+              color: '#fff',
+              backgroundColor: '#999',
+              borderRadius: 3,
+              padding: [3, 5]
+            }
+          },
+          indicator: [
+            { name: '销售（sales）', max: 6500 },
+            { name: '管理（Administration）', max: 16000 },
+            { name: '信息技术（Information Techology）', max: 30000 },
+            { name: '客服（Customer Support）', max: 38000 },
+            { name: '研发（Development）', max: 52000 },
+            { name: '市场（Marketing）', max: 25000 }
+          ]
+        },
+        series: [{
+          name: '预算 vs 开销（Budget vs spending）',
+          // type: 'radar',
+          // areaStyle: {normal: {}},
+          data: [
+            {
+              value: [4300, 10000, 28000, 35000, 50000, 19000],
+              name: '预算分配（Allocated Budget）'
+            },
+            {
+              value: [5000, 14000, 28000, 31000, 42000, 21000],
+              name: '实际开销（Actual Spending）'
+            }
+          ]
+        }]
+      };
+      myChart.setOption(option);
+    },
+    fengbtn() {
+      this.fenglist()
+    },
+    listbtns() {
+      this.collectionChart()
     }
   },
   mounted() {
@@ -129,12 +253,12 @@ export default {
 .listcontent {
   width: 100%;
   .listtop {
-    .black {
-
-    }
-    .blue {
-
-    }
+    .black {}
+    .blue {}
   }
+}
+
+.addbtn {
+  margin-left: 20px;
 }
 </style>
